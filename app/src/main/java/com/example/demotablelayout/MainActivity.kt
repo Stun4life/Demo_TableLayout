b48e5bfe0cbf734898d15314072640ef55a091df
Stun4Life <weeweiren@gmail.com>
2021-08-17 23:27:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demotablelayout/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.demotablelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun btnOnClick(view : View) {
+        var msg = ""
+        val btnSelected = view as Button
+        when(btnSelected.id) {
+            btn1.id -> msg = "button 1 is clicked"
+            btn2.id -> msg = "button 2 is clicked"
+            btn3.id -> msg = "button 3 is clicked"
+            btn4.id -> msg = "button 4 is clicked"
+            btn5.id -> msg = "button 5 is clicked"
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+}
